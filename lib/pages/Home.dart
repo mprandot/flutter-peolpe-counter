9b5cedaf961483cfa6df70cb02b0a2a2e2b1ed36
Márcio Prando Teixeira <mprandot@gmail.com>
2020-08-08 13:40:52 -0300

    Use Scaffold

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -25,55 +25,57 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xff612F74),
-      child: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Pessoas - $_people", textDirection: TextDirection.ltr, 
-                style: TextStyle(
-                  color: Colors.white, 
-                  fontWeight: FontWeight.bold
+    return Scaffold(
+      body: Container(
+        color: const Color(0xff612F74),
+        child: Stack(
+          children: <Widget>[
+            Image.asset(
+              "images/restaurant.jpg",
+              fit: BoxFit.cover,
+              height: 1000.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Pessoas - $_people", textDirection: TextDirection.ltr, 
+                  style: TextStyle(
+                    color: Colors.white, 
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: 
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: 
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlatButton(
+                        child: Text("+1", style: TextStyle(color: Colors.white, fontSize: 20)),
+                        onPressed: () {
+                          _changePeople(1);
+                        },
+                      ),
                       FlatButton(
-                      child: Text("+1", style: TextStyle(color: Colors.white, fontSize: 20)),
-                      onPressed: () {
-                        _changePeople(1);
-                      },
-                    ),
-                    FlatButton(
-                      child: Text("-1", style: TextStyle(color: Colors.white, fontSize: 20)),
-                      onPressed: () {
-                        _changePeople(-1);
-                      },
-                    ),
-                  ],
+                        child: Text("-1", style: TextStyle(color: Colors.white, fontSize: 20)),
+                        onPressed: () {
+                          _changePeople(-1);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Text("$_restaurantStatus", textDirection: TextDirection.ltr, 
-                style: TextStyle(
-                  color: Colors.white, 
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30
+                Text("$_restaurantStatus", textDirection: TextDirection.ltr, 
+                  style: TextStyle(
+                    color: Colors.white, 
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30
+                  ),
                 ),
-              ),
-            ],
-          )
-        ]
+              ],
+            )
+          ]
+        ),
       ),
     );
   }
